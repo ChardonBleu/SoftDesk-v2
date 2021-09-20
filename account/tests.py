fc48772df5b81401750f2e77a0cb89a77cd88eb1
Fabienne RONDI <f.rondi@orange.fr>
2021-09-20 08:18:35 +0200

    Tests account

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import User
+
+
+@pytest.fixture
+def api_client(db):
+    return APIClient()
+
+
+# ################################################################# #
+# ####################### TEST registration ####################### #
+
+
+def test_registration(db, api_client: APIClient) -> None:
+    user_count = User.objects.count()
+    response = api_client.post('/signup/',
+                               {'username': 'azalae',
+                                'first_name': 'elo',
+                                'last_name': 'dalb',
+                                'email': 'elo@soleneidos.fr',
+                                'password': 'choucroute'}) 
+    assert response.status_code == 201
+    assert User.objects.count() == user_count + 1
+
--- issuetracking/tests.py
+++ issuetracking/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import pytest
+import uuid
+from django.urls import reverse
+from django.test import Client
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+
+from .models import Project, Issue, Comment, Contributor
+from account.models import User
+
+
+@pytest.fixture
+def client(db):
+    return APIClient()
+
+
+   
+def test_api_jwt(client):
+    
+    url = reverse('token_obtain_pair')
+    u = User.objects.create_user(username='user', email='user@foo.com', password='pass')
+    u.is_active = False
+    u.save()
+
+    resp = client.post(url, {'email':'user@foo.com', 'password':'pass'}, format='json')
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    u.is_active = True
+    u.save()
+
+    resp = client.post(url, {'username':'user@foo.com', 'password':'pass'}, format='json')
+    assert resp.status_code == status.HTTP_200_OK
+    assert 'token' in resp.data
+    token = resp.data['token']
+    print('*********************', token)
+
+    verification_url = reverse('token_refresh')
+    resp = client.post(verification_url, {'token': token}, format='json')
+    assert resp.status_code == status.HTTP_200_OK
+
+    resp = client.post(verification_url, {'token': 'abc'}, format='json')
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='JWT ' + 'abc')
+    resp = client.get('/projects/', data={'format': 'json'})
+    assert resp.status_code == status.HTTP__UNAUTHORIZED
+    client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+    resp = client.get('/projects/', data={'format': 'json'})
+    assert resp.status_code == status.HTTP_200_OK
